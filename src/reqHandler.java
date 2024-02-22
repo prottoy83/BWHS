218defdead015caaeb09640cc3a2789d3cf75ee4
prottoy83 <prottoyroy77@gmail.com>
2024-02-23 03:30:39 +0600

    Added GET Response handling and returning of html files

--- src/reqHandler.java
+++ src/reqHandler.java
@@ -11,22 +11,30 @@ public class reqHandler {
             String method = reqs[0];
             String getFile = reqs[1]; 
 
+            //Print out the necessary infos
             System.out.printf("\nNEW CONNECTION\n");
             System.out.println(line);
             System.out.println(getFile.substring(1));
             
 
-
+            //Handle GET Requests
             if(method.equals("GET")){
-                String response = "HTTP/1.1 200 OK\r\n\r\nConnection Estabilished";
+                //if (getFile.equals("")){getFile = "index.html";}  //NOT WORKING FOR NOW 
+                String response = httpResponses.HTMLResponse(getFile);
                 OutputStream oStr = clSocket.getOutputStream();
                 oStr.write(response.getBytes());
+
+
             }
+
+            //Handles POST Requests
             else if(method.equals("POST")){
                 String response = "HTTP/1.1 501 NOT IMPLIMENTED\r\n\r\nServer Error: NOT IMPLIMENTED";
                 OutputStream oStr = clSocket.getOutputStream();
                 oStr.write(response.getBytes());
             }
+
+            //Handles Everything else (Basically returns ERROR)
             else{
                 String response = "HTTP/1.1 500 Internal Server Error\r\n\r\nServer Error: Internal Server Error";
                 OutputStream oStr = clSocket.getOutputStream();
